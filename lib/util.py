f4776d6163fec2b6bee8ae0784bf32439bf072ff
Krikyn <kirill.vakhrushev@gmail.com>
2019-12-25 20:01:12 +0300

    added all_combinations function

--- lib/util.py
+++ lib/util.py
@@ -1,4 +1,6 @@
-from expressions import *
+import itertools
+
+from lib.expressions import Num, Alpha
 
 
 def tobin(x):
@@ -11,3 +13,9 @@ def numToAlpha(x, field):
     else:
         p = field.powerOf(x)
         return Alpha(p)
+
+
+def all_combinations(list):
+    for l in range(0, len(list) + 1):
+        for subset in itertools.combinations(list, l):
+            yield subset
